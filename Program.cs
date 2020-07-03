eb7eb0c0823a373fa135b8e43c9b465f6a1ab7d1
davidkandy <davidkandy98@gmail.com>
2020-07-03 13:13:34 +0100

    Proj

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
