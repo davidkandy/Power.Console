7d1ff2f790918d6b05420baa0bf5e3dcf8d7c1aa
PrinceOwen9466 <princeowenolive@gmail.com>
2020-07-03 17:58:34 +0100

    Added PowerCountdown

---
+++ PowerCountdown.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Power.Countdown
+{
+    public class PowerCountdown
+    {
+        #region Properties
+
+        #region Statics
+        const int SECONDS_PER_MINUTE = 60;
+        // const int MINUTES_PER_HOUR = ??
+        // const int HOURS_PER_DAY = ?? <= Is this necessary
+        #endregion
+
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public int CountdownTime { get; private set; }
+
+        #endregion
+
+        #region Constructors
+        public PowerCountdown(int seconds)
+        {
+            CountdownTime = seconds;
+
+            // Convert the countdown time from seconds to hours:minute:seconds
+            Seconds = CountdownTime % SECONDS_PER_MINUTE;
+            // Minutes = ?
+            // Hours = ?
+        }
+        #endregion
+
+        #region Methods
+        public void Start()
+        {
+            while (CountdownTime > 0)
+            {
+                Thread.Sleep(1000); // Wait for 1000 milliseconds
+                CountdownTime--;
+
+                if (Seconds <= 0)
+                {
+                    if (Minutes > 0) Minutes--;
+                    else if (Hours > 0)
+                    {
+                        Minutes = 59;
+                        Hours--;
+                    }
+                }
+                else Seconds--;
+
+                Console.WriteLine($"{Hours}:{Minutes}:{Seconds}");
+            }
+
+            Console.WriteLine("Time is up!!!");
+            // Process.Start("shutdown", "/s /f /t 20");
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Power.Countdown;
+using System;
 
 namespace Power
 {
     class Program
-    
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Generate a random number each time
+            Random random = new Random();
+            int seconds = random.Next(120, 1000);
+
+            Console.WriteLine($"Hello there. I'll be counting down to {seconds}");
+            Console.WriteLine("");
+
+            var countdown = new PowerCountdown(seconds);
+            countdown.Start();
         }
     }
 }
